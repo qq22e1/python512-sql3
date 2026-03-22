bc17efdcd25f1fc97d962043b24fc5cd59882f40
Vanya <a-dimitrov@internet.ru>
2026-03-22 19:24:08 +0300

    create db courses and enrollments

--- requests_sql2.py
+++ requests_sql2.py
@@ -3,14 +3,26 @@ import sqlite3
 connect = sqlite3.connect('University')
 cursor = connect.cursor()
 
+cursor.execute("PRAGMA foreign_keys = ON;")
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS students(
                   StudentID INTEGER PRIMARY KEY AUTOINCREMENT,
                   FirstName TEXT,
-                  LastName TEXT)""")
+                  LastName TEXT
+                  )""")
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS courses(
                   CourseID INTEGER PRIMARY KEY AUTOINCREMENT,
                   CourseName TEXT,
-                  Credits INTEGER)""")
+                  Credits INTEGER
+                  )""")
 
+cursor.execute("""CREATE TABLE IF NOT EXISTS enrollments(
+                  EnrollmentID INTEGER PRIMARY KEY AUTOINCREMENT,
+                  StudentID INTEGER REFERENCES students(StudentID),
+                  CourseID INTEGER REFERENCES courses(CourseID)
+                  )""")
+
+
+connect.commit()
+connect.close()
