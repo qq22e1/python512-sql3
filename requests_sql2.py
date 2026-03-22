f154307127eb7ddfb22cfc15069244764eefcae7
Vanya <a-dimitrov@internet.ru>
2026-03-22 20:21:29 +0300

    add to db

--- requests_sql2.py
+++ requests_sql2.py
@@ -23,6 +23,36 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS enrollments(
                   CourseID INTEGER REFERENCES courses(CourseID)
                   )""")
 
+students_data = [
+    ("Иван", "Петров"),
+    ("Мария", "Сидорова"),
+    ("Алексей", "Смирнов"),
+    ("Екатерина", "Кузнецова"),
+    ("Дмитрий", "Волков")]
 
-connect.commit()
+courses_data = [
+    ("Математика", 5),
+    ("Физика", 4),
+    ("Программирование", 6),
+    ("Базы данных", 5),
+    ("Английский язык", 3)]
+
+enrollments_data = [
+    (1, 3), (1, 5),
+    (2, 1), (2, 4),
+    (3, 2), (3, 5),
+    (4, 1), (4, 3),
+    (5, 2), (5, 4)
+]
+#cursor.executemany("INSERT INTO students (FirstName, LastName) VALUES (?, ?)", students_data)
+
+#cursor.executemany("INSERT INTO courses (CourseName, Credits) VALUES (?, ?)", courses_data)
+
+#cursor.executemany("INSERT INTO enrollments (StudentID, CourseID) VALUES (?, ?)", enrollments_data)
+cursor.execute("SELECT * FROM enrollments")
+result = cursor.fetchall()
+print(result)
+
+
+#connect.commit()
 connect.close()
